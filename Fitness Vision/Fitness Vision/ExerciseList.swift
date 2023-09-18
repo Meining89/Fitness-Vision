069bb67727135d377e1f4b8b9ad97581399564cd
Emma Fu <emmafu301@hotmail.com>
2023-09-18 11:12:05 -0400

    Created list for exercises

--- Fitness Vision/Fitness Vision/ExerciseList.swift
+++ Fitness Vision/Fitness Vision/ExerciseList.swift
@@ -8,19 +8,65 @@
 import Foundation
 import SwiftUI
 
-struct ExercisesPage: View {
+struct ExerciseInfo: Identifiable{
+    let id = UUID()
+    let name: String
+    let description: String
+}
+
+struct ExerciseRow: View {
+    @State private var isExpanded = false
+    let exercise: ExerciseInfo
+    
     var body: some View {
+        VStack(alignment: .leading) {
+            Button(action: {
+                isExpanded.toggle()
+            }) {
+                Text(exercise.name)
+                    .font(.headline)
+            }
+            
+            if isExpanded {
+                Text(exercise.description)
+                    .font(.body)
+                    .foregroundColor(.secondary)
+            }
+        }
+    }
+}
+
+struct ExercisesPage: View {
+    
+    init() {
+            // Use this if NavigationBarTitle is with Large Font
+            UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+
+            // Use this if NavigationBarTitle is with displayMode = .inline
+            UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.white]
+    }
+    
+    var body: some View {
+        // Hardcoded exercise list
+        let exerciseList: [ExerciseInfo] = [
+            ExerciseInfo(name: "Push Up", description: "Push up description"),
+            ExerciseInfo(name: "Squat", description: "Squat description")
+        ]
+        
+        
         NavigationView {
             ZStack{
                 LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .topLeading, endPoint: .bottomTrailing)
                     .edgesIgnoringSafeArea(.all)
+                
                 VStack{
-                    Text("**Exercises**")
-                        .foregroundStyle(.white)
-                        .font(.largeTitle)
-                        .multilineTextAlignment(.center)
-                        .padding()
+                    List(exerciseList) { exercise in
+                        ExerciseRow(exercise: exercise)
+                    }
+                    .scrollContentBackground(.hidden)
+                    .navigationTitle("Available Exercises")
                     Spacer()
+                    
                 }
             }
         }
