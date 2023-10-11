bd382a71126b591a36fe9f2679f99e96a9b6c189
Emma Fu <emmafu301@hotmail.com>
2023-10-11 18:43:18 -0400

    App can ask permission and show front camera

--- Fitness Vision/Fitness Vision/ViewController.swift
+++ Fitness Vision/Fitness Vision/ViewController.swift
@@ -1,111 +1,102 @@
-//
-//  CameraViewController.swift
-//  Fitness Vision
-//
-//  Created by Emma Fu on 2023-10-11.
-//
-import Foundation
-import SwiftUI
-import CoreData
-import AVFoundation
 import UIKit
+import SwiftUI
+import AVFoundation
 
-// https://github.com/barbulescualex/iOSCustomCamera/blob/master/Starter/CustomCamera/ViewController%2BExtras.swift
-// https://www.youtube.com/watch?v=ZYPNXLABf3c&ab_channel=iOSAcademy
 
-
-class CameraViewController: UIViewController {
-    var captureSession : AVCaptureSession?
-    
-    var backCamera : AVCaptureDevice?
-    var frontCamera : AVCaptureDevice?
-    var backInput : AVCaptureInput?
-    var frontInput : AVCaptureInput?
-    
-    var previewLayer : AVCaptureVideoPreviewLayer?
-    var videoOutput : AVCaptureVideoDataOutput?
-    
+class ViewController: UIViewController {
+    private var permissionGranted = false // Flag for permission
+    private let captureSession = AVCaptureSession()
+    private let sessionQueue = DispatchQueue(label: "sessionQueue")
+    private var previewLayer = AVCaptureVideoPreviewLayer()
+    var screenRect: CGRect! = nil // For view dimensions
+      
     override func viewDidLoad() {
-        super.viewDidLoad()
+        checkPermission()
         
-        // Request camera permission
-        checkCameraPermission()
-        
-        // Set up and start the camera capture session
-        setupCamera()
-    }
-    
-    private func checkCameraPermission(){
-        switch AVCaptureDevice.authorizationStatus(for: .video) {
-        case .authorized:
-            // Camera access already granted
-            return
-        case .notDetermined:
-            // Request camera permission
-            AVCaptureDevice.requestAccess(for: .video) { [weak self] granted in
-                if granted {
-                    // Permission granted, setup camera
-                    self?.setupCamera()
-                } else {
-                    // Permission denied, handle appropriately
-                    return
-                }
-            }
-        case .denied, .restricted:
-            // Camera access denied or restricted, handle appropriately
-            return
-        @unknown default:
-            return
+        sessionQueue.async { [unowned self] in
+            guard permissionGranted else { return }
+            self.setupCaptureSession()
+            self.captureSession.startRunning()
         }
     }
     
-    private func setupCamera(){
-        guard let captureSession = captureSession else { return }
-        
-        // Define the camera input
-        
-        // Currently only front camera
-        // For all camera, use line below
-        // if let device = AVCaptureDevice.default(for: .video)
-        if let frontCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) {
-            do {
-                let cameraInput = try AVCaptureDeviceInput(device: frontCamera)
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        screenRect = UIScreen.main.bounds
+        self.previewLayer.frame = CGRect(x: 0, y: 0, width: screenRect.size.width, height: screenRect.size.height)
+
+        switch UIDevice.current.orientation {
+            // Home button on top
+            case UIDeviceOrientation.portraitUpsideDown:
+                self.previewLayer.connection?.videoOrientation = .portraitUpsideDown
+             
+            // Home button on right
+            case UIDeviceOrientation.landscapeLeft:
+                self.previewLayer.connection?.videoOrientation = .landscapeRight
+            
+            // Home button on left
+            case UIDeviceOrientation.landscapeRight:
+                self.previewLayer.connection?.videoOrientation = .landscapeLeft
+             
+            // Home button at bottom
+            case UIDeviceOrientation.portrait:
+                self.previewLayer.connection?.videoOrientation = .portrait
                 
-                if captureSession.canAddInput(cameraInput) {
-                    captureSession.addInput(cameraInput)
-                    
-                    // Define the video preview layer
-                    previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-                    if let previewLayer = previewLayer {
-                        view.layer.addSublayer(previewLayer)
-                        previewLayer.videoGravity = .resizeAspectFill
-                        previewLayer.frame = view.bounds
-                    }
-                    
-                    // Start the capture session
-                    captureSession.startRunning()
-                    self.captureSession = captureSession
-                }
-            } catch {
-                // Handle any errors
-                print(error)
+            default:
+                break
             }
-        }
-        
     }
     
-    override func viewDidLayoutSubviews() {
-         super.viewDidLayoutSubviews()
-         previewLayer?.frame = view.bounds
-     }
+    func checkPermission() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+            // Permission has been granted before
+            case .authorized:
+                permissionGranted = true
+                
+            // Permission has not been requested yet
+            case .notDetermined:
+                requestPermission()
+                    
+            default:
+                permissionGranted = false
+            }
+    }
+    
+    func requestPermission() {
+        sessionQueue.suspend()
+        AVCaptureDevice.requestAccess(for: .video) { [unowned self] granted in
+            self.permissionGranted = granted
+            self.sessionQueue.resume()
+        }
+    }
+    
+    func setupCaptureSession() {
+        // Camera input
+        guard let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera,for: .video, position: .front) else { return }
+        guard let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice) else { return }
+           
+        guard captureSession.canAddInput(videoDeviceInput) else { return }
+        captureSession.addInput(videoDeviceInput)
+                         
+        // Preview layer
+        screenRect = UIScreen.main.bounds
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = CGRect(x: 0, y: 0, width: screenRect.size.width, height: screenRect.size.height)
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill // Fill screen
+        previewLayer.connection?.videoOrientation = .portrait
+        
+        // Updates to UI must be on main queue
+        DispatchQueue.main.async { [weak self] in
+            self!.view.layer.addSublayer(self!.previewLayer)
+        }
+    }
 }
 
 struct HostedViewController: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
-        return CameraViewController()
-    }
-    
-    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
-        
-    }
+        return ViewController()
+        }
+
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        }
 }
